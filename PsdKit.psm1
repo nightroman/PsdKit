fcbc5da5afa42d3972f1d9aaabdc9165868a4f7d
Steffen Kampmann <steffen.kampmann@gmail.com>
2023-06-04 13:28:19 +0200

    pipeline input for import-psd

--- PsdKit.psm1
+++ PsdKit.psm1
@@ -158,31 +158,33 @@ function Get-Psd {
 function Import-Psd {
 	[OutputType([Hashtable])]
 	param(
-		[Parameter(Position=0, Mandatory=1)]
+		[Parameter(Position=0, Mandatory=1, ValueFromPipeline=1)]
 		[string] $Path,
 		[hashtable] $MergeInto,
 		[switch] $Unsafe
 	)
-	trap {ThrowTerminatingError $_}
+	process {
+		trap {ThrowTerminatingError $_}
 
-	$Path = $PSCmdlet.GetUnresolvedProviderPathFromPSPath($Path)
-	if ($Unsafe) {
-		$block = [scriptblock]::Create([System.IO.File]::ReadAllText($Path))
-		$data = & $block
-	}
-	else {
-		$data = $null
-		Import-LocalizedData -BaseDirectory ([System.IO.Path]::GetDirectoryName($Path)) -FileName ([System.IO.Path]::GetFileName($Path)) -BindingVariable data
-	}
-
-	if ($MergeInto) {
-		if ($data -isnot [hashtable]) {throw 'With Merge imported data must be a hastable.'}
-		foreach($_ in $data.GetEnumerator()) {
-			$MergeInto[$_.Key] = $_.Value
+		$Path = $PSCmdlet.GetUnresolvedProviderPathFromPSPath($Path)
+		if ($Unsafe) {
+			$block = [scriptblock]::Create([System.IO.File]::ReadAllText($Path))
+			$data = & $block
 		}
-	}
-	else {
-		$data
+		else {
+			$data = $null
+			Import-LocalizedData -BaseDirectory ([System.IO.Path]::GetDirectoryName($Path)) -FileName ([System.IO.Path]::GetFileName($Path)) -BindingVariable data
+		}
+
+		if ($MergeInto) {
+			if ($data -isnot [hashtable]) {throw 'With Merge imported data must be a hastable.'}
+			foreach($_ in $data.GetEnumerator()) {
+				$MergeInto[$_.Key] = $_.Value
+			}
+		}
+		else {
+			$data
+		}
 	}
 }
 
